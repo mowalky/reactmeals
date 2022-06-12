8a5871170ca59285c102ec164d9a6238145d7d8a
Mowalky <mowalky@outlook.com>
2022-06-11 21:23:54 -0500

    added modal to cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,17 @@
 import classes from "./Cart.module.css";
 
+import Modal from "../UI/Modal";
+
 const Cart = (props) => {
   const cartItems = (
-    <ul className={classes.item}>
+    <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "sushi", amount: 2, price: 12.0 }].map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
   );
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -19,7 +21,7 @@ const Cart = (props) => {
         <button className={classes["button--alt"]}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
